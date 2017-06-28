7571c73fafc1f24ba4327791964fbab162df7ef3
zhuwenzhen <wenzhen.zwz@gmail.com>
2017-06-28 11:22:29 +0000

    write a script to download all datasets

---
+++ data/get_coco.sh
@@ -0,0 +1,5 @@
+wget http://msvocds.blob.core.windows.net/coco2014/val2014.zip
+wget http://msvocds.blob.core.windows.net/annotations-1-0-3/instances_train-val2014.zip
+wget http://msvocds.blob.core.windows.net/annotations-1-0-3/person_keypoints_trainval2014.zip
+wget http://msvocds.blob.core.windows.net/coco2014/test2014.zip
+wget http://msvocds.blob.core.windows.net/coco2015/test2015.zip
